fbdcd9bb5bb3abeccfdecec08cdf89a915aa4d36
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-03-06 11:40:56 -0600

    education create

--- app/controllers/api/educations_controller.rb
+++ app/controllers/api/educations_controller.rb
@@ -1,2 +1,23 @@
 class Api::EducationsController < ApplicationController
+
+
+  def create
+    @education = Education.new(
+      student_id: Student.first.id, 
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      university_name: params[:university_name],
+      degree: params[:degree],
+      details: params[:details])
+
+    if @education.save
+      render "show.json.jb"
+    else
+      render json: {errors: @education.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
 end
