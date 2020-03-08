078ef354ad2eccfd40bf2197bad2ee584a640a62
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-03-08 18:19:40 -0500

    authentications

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,4 +1,6 @@
 class Api::ProjectsController < ApplicationController
+  before_action :authenticate_student, except: [:index, :show]
+  
   def create
     @project = Project.new(
                            name: params[:name],
--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,22 +1,18 @@
 class Api::SessionsController < ApplicationController
-  # def create
-#     puts "=" * 30
-#     p params[:email]
-#     p params[:password]
-#     puts "=" * 30
-#     student = Student.find_by(email: params[:email])
-#     if student && student.authenticate(params[:password])
-#       jwt = JWT.encode(
-#         {
-#           student_id: student.id, # the data to encode
-#           exp: 24.hours.from_now.to_i # the expiration time
-#         },
-#         Rails.application.credentials.fetch(:secret_key_base), # the secret key
-#         "HS256" # the encryption algorithm
-#       )
-#       render json: { jwt: jwt, email: student.email, student_id: student.id }, status: :created
-#     else
-#       render json: {}, status: :unauthorized
-#     end
-  # end
+  def create
+    student = Student.find_by(email: params[:email])
+    if student && student.authenticate(params[:password])
+      jwt = JWT.encode(
+        {
+          student_id: student.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        Rails.application.secrets.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+      render json: { jwt: jwt, email: student.email, student_id: student.id }, status: :created
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
 end
